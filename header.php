4c39e494dd24719fa02c71337beadf807286cb58
Daniel Ryan <d@nielryan.com>
2020-04-01 08:09:56 -0600

    Updated favicons

--- header.php
+++ header.php
@@ -34,6 +34,20 @@
         ?>
 	</title>
 
+	<link rel="apple-touch-icon" sizes="180x180"
+		href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32"
+		href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16"
+		href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png">
+	<link rel="manifest"
+		href="<?php echo get_template_directory_uri(); ?>/favicon/site.webmanifest">
+	<link rel="mask-icon"
+		href="<?php echo get_template_directory_uri(); ?>/favicon/safari-pinned-tab.svg"
+		color="#00a6fc">
+	<meta name="msapplication-TileColor" content="#324659">
+	<meta name="theme-color" content="#ffffff">
+
 	<script type="text/javascript">
 		var pathInfo = {
 			base: '<?php echo get_template_directory_uri(); ?>/',
